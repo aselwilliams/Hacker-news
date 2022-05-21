e30ffd1349cf2286ed5b6a07c6f8877fcd0d8b35
aselwilliams <aselwilliams22@gmail.com>
2022-05-21 16:26:56 -0400

    removed localstorage

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import axios from "axios";
 import "./App.css";
-import Modal from './components/Modal'
-import React, { useState, useEffect} from "react";
+import Modal from "./components/Modal";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 import LoadingIcon from "./components/LoadingIcon";
 import NewsFeed from "./components/NewsFeed";
 const api = "https://hn.algolia.com/api/v1/search?query=";
-const userUrl='https://hn.algolia.com/api/v1/users/'
+const userUrl = "https://hn.algolia.com/api/v1/users/";
 
 function App() {
- const [news, setNews]= useState([]);
- const [isLoading, setIsLoading] =useState(false);
- const [searchValue, setSearchValue] = useState('');
- const [url, setUrl] = useState(api);
- const [modal, setModal] =useState(false);
- const [userInfo, setUserInfo] = useState({});
- const [newUrl, setNewUrl]=useState(userUrl)
-  
+  const [news, setNews] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [url, setUrl] = useState(api);
+  const [modal, setModal] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  const [newUrl, setNewUrl] = useState(userUrl);
+
   const getData = () => {
-    axios
-      .get(url)
-      .then((data) => setNews(data.data.hits));
+    axios.get(url).then((data) => setNews(data.data.hits));
   };
 
-  useEffect(()=>{
-     setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
     getData();
     setIsLoading(false);
-  },[])
+  }, []);
 
-  useEffect(()=>{
-      getData();
-  },[url])
+  useEffect(() => {
+    getData();
+  }, [url]);
 
   const handleDelete = (id) => {
     const updatedList = news.filter((item) => item.objectID !== id);
@@ -44,44 +42,52 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-      setUrl(`${api}${searchValue}`)
-    };
-  
-    const showUserInfo=(username)=>{
-      console.log(username)
-      axios
-      .get(`${userUrl}${username}`)
-      .then((data) =>
-      setUserInfo(data.data));
-      setModal(true)
-      console.log(userInfo)
-    }
+    setUrl(`${api}${searchValue}`);
+    saveToLocalStorage(news);
+  };
 
-    const closeModal=()=>{
-      setModal(false)
-    }
-    return (
-      <>
-        <div className="container">
-          <header>
-            <h1>Hacker News</h1>
-          </header>
-          <Form
-            handleSubmit={handleSubmit}
-            handleInputChange={handleInputChange}
-          />
-          {modal ? <Modal userInfo={userInfo} closeModal={closeModal} />
-          : <>
-          {isLoading ? (
-            <LoadingIcon />
-          ) : (
-            <NewsFeed news={news} handleDelete={handleDelete} showUserInfo={showUserInfo} />
-          )}
- </> }
-        </div>
-      </>
-    );
-  
+  const showUserInfo = (username) => {
+    console.log(username);
+    axios.get(`${userUrl}${username}`).then((data) => setUserInfo(data.data));
+    setModal(true);
+    console.log(userInfo);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const saveToLocalStorage = (news) => {
+    localStorage.setItem("hacker-news-searchValue", JSON.stringify(news));
+  };
+  return (
+    <>
+      <div className="container">
+        <header>
+          <h1>Hacker News</h1>
+        </header>
+        <Form
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+        />
+        {modal ? (
+          <Modal userInfo={userInfo} closeModal={closeModal} />
+        ) : (
+          <>
+            {isLoading ? (
+              <LoadingIcon />
+            ) : (
+              <NewsFeed
+                news={news}
+                handleDelete={handleDelete}
+                showUserInfo={showUserInfo}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
